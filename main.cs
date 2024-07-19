094ba606b2ec90fb09781f8a3dab03a36331f758
Mirjamol Mirislomov <96412090+holmon-alp@users.noreply.github.com>
2024-07-19 14:57:42 +0500

    Update main.cs

--- main.cs
+++ main.cs
@@ -2,6 +2,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        System.Console.WriteLine("Hello, World!");
+        System.Console.WriteLine("In Github code!");
     }
 }
